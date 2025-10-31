f91593aa0e35614371bfb486f702ccd629f612d6
MahiliFlores <mahiliflores@outlook.com>
2025-10-31 15:56:14 -0600

    Actualización de mercadolibre_page.js

--- pages/mercadolibre_page.js
+++ pages/mercadolibre_page.js
@@ -1,6 +1,5 @@
 const { I } = inject();
-const fs = require('fs');
-const allure = codeceptjs.container.plugins('allure');
+
 
 class mercadolibre_page {
   fields = {
@@ -24,9 +23,7 @@ class mercadolibre_page {
 
   Home() {
     I.amOnPage('/');
-    I.saveScreenshot('01-Page.png', true);
-    const img = fs.readFileSync('output/01-Page.png');
-    allure.addAttachment('01-Page', img, 'image/png');
+
 
   }
 
@@ -59,9 +56,7 @@ class mercadolibre_page {
     I.waitForElement(this.fields.BarraBusqueda, 10);
     I.fillField(this.fields.BarraBusqueda, 'playstation 5');
     I.pressKey('Enter');
-    I.saveScreenshot('03-Search.png', true);
-    const img = fs.readFileSync('output/03-Search.png');
-    allure.addAttachment('03-Search', img, 'image/png');
+
 
   }
 
@@ -71,9 +66,7 @@ class mercadolibre_page {
     I.scrollTo(this.fields.Condicion);
     I.wait(1);
     I.click(this.fields.CondicionNuevo);
-    I.saveScreenshot('04-Filter.png', true);
-    const img = fs.readFileSync('output/04-Filter.png');
-    allure.addAttachment('04-Filter', img, 'image/png');
+
 
   }
 
@@ -81,9 +74,7 @@ class mercadolibre_page {
   FilterCDMX() {
     I.waitForElement(this.fields.FiltroLocal, 10);
     I.click(this.fields.FiltroLocal);
-    I.saveScreenshot('05-FilterCDMX.png', true);
-    const img = fs.readFileSync('output/05-FilterCDMX.png');
-    allure.addAttachment('05-FilterCDMX', img, 'image/png');
+
 
 
   }
@@ -95,9 +86,7 @@ class mercadolibre_page {
     I.wait(1);
     I.click(this.fields.OrdenarPor);
     I.click(this.fields.MayorPrecio);
-    I.saveScreenshot('06-Order.png', true);
-    const img = fs.readFileSync('output/06-Order.png');
-    allure.addAttachment('06-Order', img, 'image/png');
+
 
   }
 
@@ -119,9 +108,7 @@ class mercadolibre_page {
         price: prices[i]
       });
     }
-    await I.saveScreenshot('07-Obtain.png', true);
-    const img = fs.readFileSync('output/07-Obtain.png');
-    allure.addAttachment('07-Obtain', img, 'image/png');
+
 
     return first5;
 
@@ -134,9 +121,7 @@ class mercadolibre_page {
     // Recorrer el arreglo de productos y mostrarlos en consola 
     first5.forEach((p, i) => console.log('${i + 1}. ${p.title} - $${p.price}'));
 
-    await I.saveScreenshot('08-Print.png', true);
-    const img = fs.readFileSync('output/08-Print.png');
-    allure.addAttachment('08-Print', img, 'image/png');
+    
   }
 
 }
