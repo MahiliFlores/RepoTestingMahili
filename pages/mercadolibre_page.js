d45cb8e280983010f10fd627eadc8df77b3bd8b4
MahiliFlores <mahiliflores@outlook.com>
2025-10-31 14:58:58 -0600

    Agregue los cambios del page para las capturas y codecept config

--- pages/mercadolibre_page.js
+++ pages/mercadolibre_page.js
@@ -1,4 +1,6 @@
 const { I } = inject();
+const fs = require('fs');
+const allure = codeceptjs.container.plugins('allure');
 
 class mercadolibre_page {
   fields = {
@@ -19,8 +21,12 @@ class mercadolibre_page {
 
 
   };
+
   Home() {
     I.amOnPage('/');
+    I.saveScreenshot('01-Page.png', true);
+    const img = fs.readFileSync('output/01-Page.png');
+    allure.addAttachment('01-Page', img, 'image/png');
 
   }
 
@@ -43,6 +49,9 @@ class mercadolibre_page {
     } catch (err) {
 
     }
+    await I.saveScreenshot('02-Country.png', true);
+    const img = fs.readFileSync('output/02-Country.png');
+    allure.addAttachment('02-Country', img, 'image/png');
   }
 
   Search() {
@@ -50,6 +59,9 @@ class mercadolibre_page {
     I.waitForElement(this.fields.BarraBusqueda, 10);
     I.fillField(this.fields.BarraBusqueda, 'playstation 5');
     I.pressKey('Enter');
+    I.saveScreenshot('03-Search.png', true);
+    const img = fs.readFileSync('output/03-Search.png');
+    allure.addAttachment('03-Search', img, 'image/png');
 
   }
 
@@ -59,12 +71,20 @@ class mercadolibre_page {
     I.scrollTo(this.fields.Condicion);
     I.wait(1);
     I.click(this.fields.CondicionNuevo);
+    I.saveScreenshot('04-Filter.png', true);
+    const img = fs.readFileSync('output/04-Filter.png');
+    allure.addAttachment('04-Filter', img, 'image/png');
+
   }
 
 
   FilterCDMX() {
     I.waitForElement(this.fields.FiltroLocal, 10);
     I.click(this.fields.FiltroLocal);
+    I.saveScreenshot('05-FilterCDMX.png', true);
+    const img = fs.readFileSync('output/05-FilterCDMX.png');
+    allure.addAttachment('05-FilterCDMX', img, 'image/png');
+
 
   }
 
@@ -75,6 +95,10 @@ class mercadolibre_page {
     I.wait(1);
     I.click(this.fields.OrdenarPor);
     I.click(this.fields.MayorPrecio);
+    I.saveScreenshot('06-Order.png', true);
+    const img = fs.readFileSync('output/06-Order.png');
+    allure.addAttachment('06-Order', img, 'image/png');
+
   }
 
 
@@ -95,6 +119,9 @@ class mercadolibre_page {
         price: prices[i]
       });
     }
+    await I.saveScreenshot('07-Obtain.png', true);
+    const img = fs.readFileSync('output/07-Obtain.png');
+    allure.addAttachment('07-Obtain', img, 'image/png');
 
     return first5;
 
@@ -102,12 +129,16 @@ class mercadolibre_page {
   }
   async Print() {
     const first5 = await this.Obtain();
-    console.log(" Estos son los primeros 5 productos encontrados:");
-    //Recorrer el arreglor de productos y mostrarlos en consola 
-    first5.forEach((p, i) => console.log(`${i + 1}. ${p.title} - $${p.price}`));
+    console.log("Estos son los primeros 5 productos encontrados:");
 
+    // Recorrer el arreglo de productos y mostrarlos en consola 
+    first5.forEach((p, i) => console.log('${i + 1}. ${p.title} - $${p.price}'));
+
+    await I.saveScreenshot('08-Print.png', true);
+    const img = fs.readFileSync('output/08-Print.png');
+    allure.addAttachment('08-Print', img, 'image/png');
   }
-}
-module.exports = new mercadolibre_page(); 
 
-//cambio para generar un pull
+}
+
+module.exports = new mercadolibre_page();
